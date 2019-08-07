0535b39fd806d9dbd344177b031b543710757514
Jeremy Yung Hau Ng <jeremy.ng1002@gmail.com>
2019-08-07 14:47:37 +1000

    start implementing dashboard page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,30 @@ import { environment } from "../environments/environment";
 
 import { AppComponent } from "./app.component";
 import { MaterialModule } from "./material.module";
-import { DashboardComponent } from "./pages/dashboard/dashboard.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { LoginComponent } from './login/login.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { LayoutModule } from '@angular/cdk/layout';
+import { NavigationPaneComponent } from './dashboard/components/navigation-pane/navigation-pane.component';
 
 @NgModule({
-  declarations: [AppComponent, DashboardComponent],
+  declarations: [AppComponent, DashboardComponent, LoginComponent, NavigationPaneComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    environment.production ? [] : AkitaNgDevtools.forRoot()
+    environment.production ? [] : AkitaNgDevtools.forRoot(),
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/dashboard/components/navigation-pane/navigation-pane.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navigation-pane',
+  templateUrl: './navigation-pane.component.html',
+  styleUrls: ['./navigation-pane.component.scss']
+})
+export class NavigationPaneComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
